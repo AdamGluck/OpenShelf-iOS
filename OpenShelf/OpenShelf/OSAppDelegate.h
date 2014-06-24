031f67ea3d1485e832552e6b0373652ac7fafb61
Brian Strobach <brianstrobach@gmail.com>
2014-06-24 15:04:26 -0500

    -testing version control

---
+++ OpenShelf/OpenShelf/OSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OSAppDelegate.h
+//  OpenShelf
+//
+//  Created by Brian Strobach on 6/24/14.
+//  Copyright (c) 2014 OpenShelf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenShelf/OpenShelf/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenShelf
+//
+//  Created by Brian Strobach on 6/24/14.
+//  Copyright (c) 2014 OpenShelf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OSAppDelegate class]));
+    }
+}
