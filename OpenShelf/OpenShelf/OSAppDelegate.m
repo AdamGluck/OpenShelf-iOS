53e7ecc54b86637c3c6670dc89bce237473720bb
Brian Strobach <brianstrobach@gmail.com>
2014-06-25 17:11:47 -0500

    -Drawer controller

--- OpenShelf/OpenShelf/OSAppDelegate.m
+++ OpenShelf/OpenShelf/OSAppDelegate.m
@@ -7,16 +7,33 @@
 //
 
 #import "OSAppDelegate.h"
+#import "ICSColorsViewController.h"
+#import "ICSDrawerController.h"
+#import "ICSPlainColorViewController.h"
 
 @implementation OSAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
+    
+    NSArray *colors = @[[UIColor colorWithRed:237.0f/255.0f green:195.0f/255.0f blue:0.0f/255.0f alpha:1.0f],
+                        [UIColor colorWithRed:237.0f/255.0f green:147.0f/255.0f blue:0.0f/255.0f alpha:1.0f],
+                        [UIColor colorWithRed:237.0f/255.0f green:9.0f/255.0f blue:0.0f/255.0f alpha:1.0f]
+                        ];
+    
+    ICSColorsViewController *colorsVC = [[ICSColorsViewController alloc] initWithColors:colors];
+    ICSPlainColorViewController *plainColorVC = [[ICSPlainColorViewController alloc] init];
+    plainColorVC.view.backgroundColor = colors[0];
+    
+    ICSDrawerController *drawer = [[ICSDrawerController alloc] initWithLeftViewController:colorsVC
+                                                                     centerViewController:plainColorVC];
+    
+    self.window.rootViewController = drawer;
     [self.window makeKeyAndVisible];
     return YES;
+
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
